4a82a8f553193d05ebc960b4dc222488147070e8
Jim <jmb15@williams.edu>
2023-12-30 06:41:40 -0800

    asdf

---
+++ cs345.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define real double
+#define GL_REAL GL_DOUBLE
+
+#include "codebase/snail.cpp"
+#include "codebase/cow.cpp"
+
+#include <iostream>
+#include <fenv.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <utility>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
