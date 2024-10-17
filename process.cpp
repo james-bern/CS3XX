2df7c627fe85fa74183930f3a6a4c515f675a109
NathanVosburg <nathanjvosburg@gmail.com>
2024-10-17 13:41:12 -0400

    CHANGING FILLET TO SECOND CLICK (DISCUSS)

--- process.cpp
+++ process.cpp
@@ -1154,7 +1154,7 @@ StandardEventProcessResult _standard_event_process_NOTE_RECURSIVE(Event event) {
                             if (_F.success) {
                                 Entity *E = two_click_command->entity_closest_to_first_click;
                                 Entity *F = _F.closest_entity;
-                                cookbook.attempt_fillet_ENTITIES_GET_DELETED_AT_END_OF_FRAME(E, F, average_click, popup->fillet_radius);
+                                cookbook.attempt_fillet_ENTITIES_GET_DELETED_AT_END_OF_FRAME(E, F, second_click, popup->fillet_radius);
                                 two_click_command->awaiting_second_click = false;
                             }
                         } else if (state_Draw_command_is_(DogEar)) {
