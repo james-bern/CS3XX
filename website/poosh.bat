045699cb781f97680fd745c91c6892b1d3cd24b2
Jim <jmb15@williams.edu>
2024-11-12 08:04:05 -0500

    asdf

--- website/poosh.bat
+++ website/poosh.bat
@@ -1,5 +1,7 @@
 cp -f -r ./* ../../james-bern.github.io/
-cd ../../james-bern.github.io/
+cd ..
+exec ./poosh.bat
+cd ../james-bern.github.io/
 git add --all
 git commit -m "CS3XX poosh.bat"
 git push
